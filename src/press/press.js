cb32e467c13f958b5ec610fe4529c90d859146c9
Alex-vvhite <xodyrev.aleksej42@gmail.com>
2023-06-21 04:09:13 +0300

    Add files via upload

---
+++ src/press/press.js
@@ -0,0 +1,128 @@
+const press = [
+    {
+        "exercies_name":"1. Подъем корпуса лежа",
+        "src":"https://1.downloader.disk.yandex.ru/preview/88589320417bc58e86284a261d848af597aa8b836635f9b82a8aabafdb7edbf1/inf/ShWFpX-w1_sEUxp6-PP8_Aryv_SqG3fhxKC0JK4B-KzPqJDqyzllxF2k8dU7tPZ0rhvL02KQUUMt_8T0ywhONw%3D%3D?uid=1687838051&filename=press_1.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Подъем корпуса лежа — это популярное упражнение с весом собственного тела для проработки прямых мышц живота. Его много лет используют как часть военных, тактических и школьных фитнес-тестов."
+    },
+    {
+        "exercies_name":"2. Планка на локтях",
+        "src":"https://2.downloader.disk.yandex.ru/preview/f346ced7b8448251d786d55ec1d2876c429b6ea06bd10987a32c54cbd01152e3/inf/R27oX0stFPV6Hzrs5PBc6fhG8u5BNbymfy61Iw5riAeaJ0xTnSVgkPmpcoAt-oMlUDAmBSZQ6qOLIgPI68jH3g%3D%3D?uid=1687838051&filename=press_2.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Планка на локтях — это еще одна разновидность планки, вовлекающая в работу практически все мышцы тела. Благодаря статической нагрузке она прорабатывает пресс, ноги мышцы спины и верхнего плечевого пояса."
+    },
+    {
+        "exercies_name":"3. Боковая планка",
+        "src":"https://3.downloader.disk.yandex.ru/preview/bab40972951e24ab7bdb6ddee6f12cbc3757f36c5bdc7019530f9ec46a38a384/inf/Zhk63qNj-sZ5ohaAh5BMaTJzg9-TFzUPstR5WhaNPUJUY7cgSL3QV3VVXi1MWThP8mbrxmlGoX4-t9WZOYRY3w%3D%3D?uid=1687838051&filename=press_3.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Боковая планка – это изометрическое упражнение для проработки мышц брюшного пресса, главную нагрузку в котором получают косые и поперечные мышцы живота. Выполняется на время и используется как в разминке, так и в основной части тренировки."
+    },
+    {
+        "exercies_name":"4. Косые скручивания лежа на боку",
+        "src":"https://3.downloader.disk.yandex.ru/preview/50faeed605b8ad01242fa848a11f2d62335c5360b9b319e993ebebdbc5551d55/inf/BLME90huawmu2BTzFYaW2DJzg9-TFzUPstR5WhaNPUK_r9xpBd_7amfLlJtf3_ulerLL0GordIFz7HYbRPO1Lg%3D%3D?uid=1687838051&filename=press_4.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Диагональные или косые скручивания — это упражнение с весом собственного тела для проработки боковых мышц живота. Упражнение можно выполнять на количество повторений, до усталости или отказа, или как часть тренировки мышц пресса."
+    },
+    {
+        "exercies_name":"5. Подъем ног в упоре",
+        "src":"https://2.downloader.disk.yandex.ru/preview/7b5f581c825763627b72f8097ce0a5319bbf1d1cc4e433a93a643fbf3724a12a/inf/ZcoIzEanhff9Bl678QjI-DJzg9-TFzUPstR5WhaNPULdvgkfnVC0em4ZzkuJJOQsNp1_Y0Ic9Pe92i3WOj4N1A%3D%3D?uid=1687838051&filename=press_5.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Подъем ног в упоре — это упражнение для проработки пресса и сгибателей бедра. Выполняется в специальной стойке, в которой руки размещают на мягких упорах, при этом корпус остается в висе."
+    },
+    {
+        "exercies_name":"6. Подъем ног в висе",
+        "src":"https://1.downloader.disk.yandex.ru/preview/f949781cdce67732e4d83d1ba0b01a4b4677a92561c20d810d7926b980c1b351/inf/46DB6gV4ulAft2kYnr8one7xKGnZMJ5d0XKsTvHntXOyb0vnd_3d4l6RjKj06EobOX1kwS-TLvmVk7CwE6-RbQ%3D%3D?uid=1687838051&filename=press_6.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Подъем ног в висе — это упражнение для проработки пресса и мышц-сгибателей бедра. Вместо того, чтобы положить предплечья на упоры тренажера, вы держитесь за перекладину."
+    },
+    {
+        "exercies_name":"7. Боковые наклоны с гантелей стоя",
+        "src":"https://2.downloader.disk.yandex.ru/preview/9918ea3990baa4c794cd96e8853319f6436c39f373358ee062013d56b715b36b/inf/C8BAAapVPwNz9gXDMDgE3-7xKGnZMJ5d0XKsTvHntXM319V6NgyZAQCdKULQNKnb6uSkPkBecYtEpevn9kIxUQ%3D%3D?uid=1687838051&filename=press_7.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Боковые наклоны с гантелей — это популярное упражнение для проработки внутренних и внешних косых мышц живота. Они помогают поддерживать осанку, защищая позвоночник. А вместе с развитыми прямыми мышцами придают очертаниям пресса завершенный, эстетичный вид."
+    },
+    {
+        "exercies_name":"8. Обратная планка",
+        "src":"https://3.downloader.disk.yandex.ru/preview/a9db13e0fd4bef440e5acfee9621aa5018745ac6a16638683e233ccd8d513226/inf/L9WWgNMmHQEovcSmuGVU1-7xKGnZMJ5d0XKsTvHntXOcmdjCfQmPtUWh4EfQ3G4oocwP_GO0bj-GKd8bZfBEuw%3D%3D?uid=1687838051&filename=press_8.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Обратная планка — вариант выполнения известного упражнения, который не уступает по эффективности классическому. Направлена на укрепление мышц кора. Выполняется в статике и не способствует мышечной гипертрофии, зато обладает другими полезными свойствами."
+    },
+    {
+        "exercies_name":"9. Двойные скручивания",
+        "src":"https://2.downloader.disk.yandex.ru/preview/d6f3717332d06b0126ad51b5e5eb3c3edc9dd8e8dde4eff3b8acdad05326babe/inf/qVplV4AUZ779m4oxytrImu7xKGnZMJ5d0XKsTvHntXO4lcFYS_Kz10AmTaS3MwIYX54M65Oo3TfcaJua0Cp65g%3D%3D?uid=1687838051&filename=press_9.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Двойные скручивания — это упражнение с собственным весом для проработки прямой мышцы живота. Оно включает подъем коленей и локтей на встречу друг другу, при этом поясница остается прижатой к полу."
+    },
+    {
+        "exercies_name":"10. Боковые скручивания",
+        "src":"https://2.downloader.disk.yandex.ru/preview/fbdb6d405ea513d529a3eb88400d9d0e6e022c2551b000a79bb41903cc1c6d43/inf/6YTn35WbJHvdiz33iHSUlQryv_SqG3fhxKC0JK4B-Kwf0v-qyZkTjLhdZv351aUXXUJyBiylLZ0IjZr1jGo-dg%3D%3D?uid=1687838051&filename=press_10.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Боковые скручивания лежа — упражнение, нацеленное на проработку косых мышц живота. Если вы хотите сделать ваш пресс максимально эстетичным, обязательно обратите не него внимание."
+    },
+    {
+        "exercies_name":"11. Подъем ног лежа",
+        "src":"https://4.downloader.disk.yandex.ru/preview/7606c7efb6d3b6b6a7c65e3f81464d4a15fe3fedf5f187f4243fab7267b3faa1/inf/HKC8ZXCgAaS59je2un8qKAryv_SqG3fhxKC0JK4B-Kyp6VvJI7miYENlWq4BNnQA4ZiMTq94mNVkZlUA8SGD4A%3D%3D?uid=1687838051&filename=press_11.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Подъем ног лежа на спине — это популярное упражнение с весом собственного тела для проработки прямой мышцы живота. Выполняется на время или количество повторений, как часть тренировки пресса."
+    },
+    {
+        "exercies_name":"12. Мельница",
+        "src":"https://2.downloader.disk.yandex.ru/preview/45590ac76d9cf844746caf604ee198291890d29e63108f67ea9a61ba5557016f/inf/5EcfFNjZWzLHG0HcderyZAryv_SqG3fhxKC0JK4B-KxvNa-SVt9T0CH5zFBHtQzwr3Wh0rY0zUJA2EtziiEUfg%3D%3D?uid=1687838051&filename=press_12.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Упражнение Мельница — на первый взгляд достаточно простое движение, известное многим из уроков физкультуры. Оно используется как элемент разминки, без использования инвентаря, так и в качестве основного движения для прокачки мышц, если добавить дополнительное отягощение."
+    },
+    {
+        "exercies_name":"13. Повороты корпуса сидя со штангой",
+        "src":"https://1.downloader.disk.yandex.ru/preview/efc09876f34558c7dbef1b24f5a7b029b3751e48215a8caba486c4ab792d390d/inf/bYk6Ou4Zt2gv7hf1Dy3MRQryv_SqG3fhxKC0JK4B-KxhXsPGbWy7I0oJxPAAxg_TbYGjaoSBk6cqJo6loHCDvA%3D%3D?uid=1687838051&filename=press_13.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Повороты корпуса сидя со штангой – это упражнение для мышц кора, укрепляет косые мышцы живота. Обычно выполняется в высоком диапазоне повторений с небольшим весом, пустым грифом или даже трубкой из ПВХ. Используется как в течение тренировки, так и в конце, также вне тренажерного зала, для укрепления и повышения стабильности мышц кора."
+    },
+    {
+        "exercies_name":"14. Касание стоп из положения лежа",
+        "src":"https://1.downloader.disk.yandex.ru/preview/c74cb692040e1281bf9f79a746a02335e132c833c90bafcea9880863ab1a12b6/inf/OYI-Vx6gYgvG2aHlzt2VvPhG8u5BNbymfy61Iw5riAd0-5Ln6NdIwu8h3nixRpsuc8qH6ixqC6JOnI3SBfFYQw%3D%3D?uid=1687838051&filename=press_14.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Касание стоп из положения лежа – это упражнение с весом собственного тела для проработки мышц брюшного пресса, непосредственно прямой мышцы живота. Можно выполнять на время или количество повторений, как часть тренировки пресса."
+    },
+    {
+        "exercies_name":"15. Обратные скручивания на скамье",
+        "src":"https://3.downloader.disk.yandex.ru/preview/8a627e2d9d7f5f05015163d1713dfc820e428abaa165a8b5cdb58301488d1aaf/inf/r-8xDm2U7bcFhzK4BjhJX_hG8u5BNbymfy61Iw5riAf7itPF0YOAU0CKD816t6jC8x43bVJ1xQo2-JCkc0YZEA%3D%3D?uid=1687838051&filename=press_15.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Обратные скручивания на скамье – это популярное упражнение на пресс, в котором подъем коленей к груди выполняется лежа на скамье. Упражнение похоже на подъем ног лежа, но возможность плотнее прижать поясницу к скамье позволит сделать технику более правильной и усилить проработку пресса. Выполняется в высоком и среднем диапазоне повторений, например 8-12 за сет, как и большинство вариантов скручиваний."
+    },
+    {
+        "exercies_name":"16. Скручивания к стопам",
+        "src":"https://2.downloader.disk.yandex.ru/preview/0171ba00d73bb1eb5c2f2853a2574fc2401c4fe4e227cf7d73f8ff3ca08d5209/inf/G49EFbEfn8KHFPVOPr-Afuj4hCGQFjnnga8XnyWM4q9XXqnTpjPI5zoLxjCFWfsPJ4hXD_-Z52h_DvFKFKG6Ug%3D%3D?uid=1687838051&filename=press_16.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Скручивания к стопам – это популярное упражнение с весом собственного тела, для проработки прямой и косых мышц живота. Можно выполнять на количество повторений или на время, как часть тренировки пресса."
+    },
+    {
+        "exercies_name":"17. Обратные скручивания на нижнем блоке",
+        "src":"https://2.downloader.disk.yandex.ru/preview/d7a5ec4bb720ef6c76262e398b6f105ba0a07fa8d0b94ba373f78eb21c81ecd3/inf/sIxLny8tPqvbdMRdPihG4WR8dxB2ZqwwHY0HHNnpTlP9ALCR8ry-0Z5Xpo1zis6EEc0LmjCY3RNZVs_V_gpF3Q%3D%3D?uid=1687838051&filename=press_17.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Обратные скручивания на нижнем блоке – это упражнение выполняется лежа на полу у нижнего блока. В таком положении прорабатывается низ мышц живота."
+    },
+    {
+        "exercies_name":"18. Дровосек",
+        "src":"https://1.downloader.disk.yandex.ru/preview/57d026e1209c14f1ffed250c1727a870e006884cf9e1051fc38df317e88d4bf2/inf/ssE8aFsdo39ugd4Qxqkc12R8dxB2ZqwwHY0HHNnpTlOLRMhHazubnCEzIV6uTThw4TQWdZP6qo0nq8zmEIYxEw%3D%3D?uid=1687838051&filename=press_18.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Упражнение «дровосек» в блочном тренажере – это движение для мышц кора, прорабатывает низ пресса и косые мышцы живота."
+    },
+    {
+        "exercies_name":"19. Боковые скручивания с поднятыми ногами",
+        "src":"https://4.downloader.disk.yandex.ru/preview/3254a4a8b4029cb52b065098169a86102e16e66cf29b8eb35246584134ed7014/inf/0Ko8c4YsBFhQeFhTOEYydfhG8u5BNbymfy61Iw5riAeS7EjU2uaJZICkDkKx-pD7ABJZFsqrCrJBL3YrUl8Vxg%3D%3D?uid=1687838051&filename=press_19.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Боковые скручивания с поднятыми ногами – это вариант скручиваний для проработки боковых мышц и прямой мышцы живота. Положив ноги на скамью, вы исключаете из движения сгибатели бедра и сосредотачиваете нагрузку на пресс. "
+    },
+    {
+        "exercies_name":"20. Ролик на пресс со штангой с ногами на скамье",
+        "src":"https://1.downloader.disk.yandex.ru/preview/87a63eeecdeabcc42391be9f56ad66eb8d3207d883ee7c9580b8c28968287846/inf/sHZNnkYSKTh7VJckl9LAsGR8dxB2ZqwwHY0HHNnpTlOIatTqMeM98bppw-43ICKYvqyzGgF7xKFQxX9DwAxk5g%3D%3D?uid=1687838051&filename=press_20.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Ролик на пресс со штангой с ногами на скамье – это достаточно сложное упражнения для проработки пресса. Похоже на обычный ролик для пресса, но со штангой можно использовать более широкую постановку рук. Постановка коленей на скамью увеличивает нагрузку на верхнюю часть тела."
+    },
+    {
+        "exercies_name":"21. Подъем коленей к груди сидя",
+        "src":"https://4.downloader.disk.yandex.ru/preview/f180b679969f65286a403e1da86632e083219caa167cf017139f5e979fe7a1bc/inf/3Y5WH8nIYI2Uowd3eDDx-WR8dxB2ZqwwHY0HHNnpTlO17d0S6jW_ccpsX6eSkEaIRAYAr5XU6kaH6Vj35Lc88A%3D%3D?uid=1687838051&filename=press_21.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":""
+    },
+    {
+        "exercies_name":"22. Подъем корпуса с жимом штанги на скамье с отрицательным уклоном",
+        "src":"https://1.downloader.disk.yandex.ru/preview/8adda911d2b1229a73fcf4aae158695b0be661c13f93e0fba4de5fb8fedb6b13/inf/MpTIljWrdO6Ou04S-DYVxUpLQMC05jD7HeRqU6sw_PdW_vhDcePCq917y9mLqwtXimaK-orlBGR4eut4Bbe4cg%3D%3D?uid=1687838051&filename=press_22.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Подъем корпуса с жимом штанги под наклоном – это упражнение для мышц пресса, для проработки прямой мышцы живота, в частности нижней части. Также задействует мышцы плеч при жиме штанги. Выполняется в среднем или высоком диапазоне повторений, например 8-12 за сет и более. Используется как часть тренировки пресса."
+    },
+    {
+        "exercies_name":"23. Планка на прямых руках",
+        "src":"https://1.downloader.disk.yandex.ru/preview/8db3af0a84fae318adf488e0d6ee8121f60b5a9896620d73cc1c7651a76bf23c/inf/uxFjm53gmvcCVSgNUrOwDQ9yb0fMAwxDYsRwMLukeCksis-pgLPddRyvhlU9crv_cFSnZGG5X4WuSqPVou5T3Q%3D%3D?uid=1687838051&filename=press_23.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Планка на прямых руках – это упражнение для тренировки мышц пресса. Выполняется из стартовой позиции для отжиманий — в упоре лежа, когда руки выпрямлены, а ладони стоят на полу."
+    },
+    {
+        "exercies_name":"24. Скручивания на верхнем блоке стоя",
+        "src":"https://4.downloader.disk.yandex.ru/preview/0ec0d8a3e8f32dcfcb00167df32573b11ffc03a025d1b668654c333d8c17e097/inf/AgwCwzfMggDj-0SVXkwUtWbS1z9NqLJOZMco6J39xOdfra0VNv2_hqHwl1obhWbrD138YXfSChGkks-oqlC28g%3D%3D?uid=1687838051&filename=press_24.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Скручивания на верхнем блоке стоя – это популярное упражнение для изоляции мышц пресса с использованием канатной рукояти. Прорабатывает прямую мышцу живота и второстепенно укрепляет глубокие мышцы кора. Выполняется в начале тренировки пресса, в среднем или высоком диапазоне повторений, например 8-12 за сет и больше."
+    },
+    {
+        "exercies_name":"25. Мельница с гирей",
+        "src":"https://4.downloader.disk.yandex.ru/preview/199714fcbc2c9f82f06a6ea773afa9b81281db5e67a26aa144db080e2ccdc72a/inf/GYWHCdEbcyWmBrA2DCY3yzJzg9-TFzUPstR5WhaNPULLMjIwbEWLLEZ1RxMW5UtNbTWfgvRxuaD_Ku6b5yNnRA%3D%3D?uid=1687838051&filename=press_25.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Модифицированный вариант традиционной «мельницы», которую часто используют в качестве разминки. Утяжеление в виде гири в поднятой руке, создает хорошее напряжение в области мышц пресса и низа спины. Большую долю нагрузки берут на себя именно косые мышцы пресса, которые в традиционных «прессовых» упражнениях обычно ею обделены. Эта особенность придает упражнению особое значение."
+    },
+
+]
