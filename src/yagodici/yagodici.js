4a80a51473035ffa991770ebd9cc726c9341ec58
Alex-vvhite <xodyrev.aleksej42@gmail.com>
2023-06-21 04:10:45 +0300

    Add files via upload

---
+++ src/yagodici/yagodici.js
@@ -0,0 +1,47 @@
+const yagodici = [
+    {
+        "exercies_name":"1. Шаг на платформу с подъемом колена",
+        "src":"https://1.downloader.disk.yandex.ru/preview/7c779e6f894908e1c24bd7946c4c46fbfceab801093d84729e82d153269f3889/inf/sIxLny8tPqvbdMRdPihG4e7xKGnZMJ5d0XKsTvHntXMXUyvqYE3G_GA8IA4fO2tWKQfJljpiDMe01jfWNcIbKw%3D%3D?uid=1687838051&filename=chagi_poochered.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Шаг на платформу с подъемом колена – это упражнение на нижнюю часть тела для проработки квадрицепсов, ягодиц и бицепса бедра. Отличается от традиционного подъема на платформу тем, что нужно полностью поднять сзади стоящую ногу до полного сгибания бедра. Таким образом увеличивается амплитуда движения и нагрузка на рабочую ногу. При работе с весом собственного тела выполняется в большом количестве повторений. Используется как часть интенсивной разминки, кардио тренировки или как часть круговой тренировки на нижнюю часть тела."
+    },
+    {
+        "exercies_name":"2. Поочередные махи ногами назад лежа",
+        "src":"https://4.downloader.disk.yandex.ru/preview/c8e85a045824d91a8fa90a4d5b80d19de0ccb9401fc96b0ffff2d12bbfb1d80a/inf/OYI-Vx6gYgvG2aHlzt2VvMuTxt7AT2QXMcOFJJIY3AKj0o3v5ryWo-JRbgrtbsUaO5NHmpN1BgyOMYj5KlRFlw%3D%3D?uid=1687838051&filename=mahi_poochered.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":""
+    },
+    {
+        "exercies_name":"3. Ягодичный мостик на фитболе",
+        "src":"https://1.downloader.disk.yandex.ru/preview/d13dbc391617abeae7c6c9f1a5d03f3475f2ee06be1e7e78c6a27381dee5ff29/inf/gUXWHHf1FkV8tZdQtPhJDu7xKGnZMJ5d0XKsTvHntXMKJgvvZ2uHRNCfcKfdtVVLzKYnhevlUTfOTc5Fm2dMxA%3D%3D?uid=1687838051&filename=most_fitball.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Ягодичный мостик на фитболе – это простое и эффективное упражнение для проработки ягодиц и бицепса бедра. Хорошая техника позволит почувствовать жжение ягодичных мышц, фитбол поможет развить правильную технику быстрее, чем скамья, которую обычно используют в упражнении. Однако, в отличие от варианта со скамьей, упражнение на мяче стоит выполнять с весом собственного тела или эспандерами, в целях безопасности. Отличное упражнение для разминки в тренировке на нижнюю часть тела либо для активации ягодиц."
+    },
+    {
+        "exercies_name":"4. Ягодичный мостик на одной ноге",
+        "src":"https://4.downloader.disk.yandex.ru/preview/587a11a67ed6526f32bfe9195399763585dcfeb7bdbb801f82d3c409ee28a960/inf/ExSdk9N9nDrWqWdQSgu1j8uTxt7AT2QXMcOFJJIY3AImDCg2n9143lvj6bnORwwKG8lcy4OyHm7YMHZBh_jYSA%3D%3D?uid=1687838051&filename=most_na_noge.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Ягодичный мостик на одной ноге – это упражнение для нижней части тела, которое прорабатывает ягодицы, низ спины и бицепс бедра. Выполняется поочередно на каждую сторону."
+    },
+    {
+        "exercies_name":"5. Ягодичный мостик со штангой",
+        "src":"https://4.downloader.disk.yandex.ru/preview/42aad9ac43ce91711ed65f0985eb295433d2121d1e43bc2718a8c170f450c010/inf/kRn-afO_jaNF9xsNO9_yG8uTxt7AT2QXMcOFJJIY3AJbhkZjool6rEjKKekfUBy4rIKrzJgToBXcffBMSSg_Vw%3D%3D?uid=1687838051&filename=most_shtanga.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Ягодичный мостик со штангой – это популярное упражнение для проработки ягодиц и бицепсов бедра. Используется как самостоятельное силовое упражнение, активационное либо разминочное в тренировке нижней части тела или как упражнение “на добивку”, которое выполняется до отказа в конце тренировки на ноги."
+    },
+    {
+        "exercies_name":"6. Отведение ноги назад в кроссовере",
+        "src":"https://1.downloader.disk.yandex.ru/preview/1279494ba6e76d7968ec535b8d5c1176a6b9547171c71d1c719c1d67052dc934/inf/6YTn35WbJHvdiz33iHSUlcuTxt7AT2QXMcOFJJIY3ALI-u0_WK39yXlhe83vfxhqxD-t9npsrhoKjmoZYy-8-w%3D%3D?uid=1687838051&filename=noga_krossover.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Отведение ноги назад в тренажере кроссовер — это изолирующее упражнение для проработки ягодиц, в особенности большой ягодичной мышцы. Движение, как правило, выполняется в среднем или высоком диапазоне повторений, например 8-12 и выше."
+    },
+    {
+        "exercies_name":"7. Отведение согнутой ноги на четвереньках",
+        "src":"https://1.downloader.disk.yandex.ru/preview/837289c15982e21b5daf080a50cf56d769ff2ee8f6b75fcb21aa41f9905845b1/inf/rNAc1xCMZokWHYdNX58-6suTxt7AT2QXMcOFJJIY3ALCiBVSYVm-wJrBMFdn9BvUqu7rBi0uBXBKtKw23s9jdg%3D%3D?uid=1687838051&filename=otvedenie.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":""
+    },
+    {
+        "exercies_name":"8. Ягодичный мостик со штангой на скамье",
+        "src":"https://3.downloader.disk.yandex.ru/preview/4504f9b632fe1218f3b4fca74202dad1a5749a3d094414269c3f86111c0f1d14/inf/a36YdzuwRUhB6sXG1k8-CQ9yb0fMAwxDYsRwMLukeCm-LYOQ1gWrG_aMKCfVFLeYG0FXDfjoCfVREztUWlfChA%3D%3D?uid=1687838051&filename=shtanga_na_skmie.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Ягодичный мостик со штангой на скамье – это упражнение, которое задействует заднюю мышечную цепь, особенно ягодичные. Выполняется полное разгибание бедер с удержанием штанги поперек таза."
+    },
+    {
+        "exercies_name":"9. Ягодичный мостик",
+        "src":"https://1.downloader.disk.yandex.ru/preview/123262e9a0dcda1a23c1edabde8ee8c40aafe13091fdb9b4092bfbaafa64b3a0/inf/6R5WWD07vEfSN3w971_OesuTxt7AT2QXMcOFJJIY3AI6mo-iy39iVOVBbDRvktlIlE0jipJ3utQ8hc034L8LAw%3D%3D?uid=1687838051&filename=yagodich_most.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Ягодичный мостик – это популярное упражнение для проработки ягодичных мышц, которое также задействует низ спины и бицепсы бедра. Идеально подходит для интенсивной разминки перед тренировкой нижней части тела или в качестве одного из базовых упражнений для проработки ягодиц."
+    },
+]
