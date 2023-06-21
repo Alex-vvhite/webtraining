1dfbc52c48b5a7cbbc7008c60c75c32929641e10
Alex-vvhite <xodyrev.aleksej42@gmail.com>
2023-06-21 04:06:22 +0300

    Add files via upload

---
+++ src/grud/grud.js
@@ -0,0 +1,122 @@
+const grud = [
+    {
+        "exercies_name":"1. Отжимания с хлопком",
+        "src":"https://2.downloader.disk.yandex.ru/preview/547013b347a54b1f9bde04f74074bca302a5e236159546b8b88df2892f6a51f0/inf/m-UgyOXPdWwjPVIdt3mD02QTKQLFYftcKK2Wf7GSZ5UMHDd4ADzYQGfX8WfEudfXKfomc3GcvauanW5JWzTw6g%3D%3D?uid=1687838051&filename=1.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Отжимания с хлопком, так же известные как плио-отжимания — это упражнение с собственным весом, направленное на увеличение взрывной и общей силы мышц верхней части тела. Прорабатывает мышцы груди, трицепс и плечи. Выполняется в малом диапазоне повторений для развития взрывной и общей силы, либо с большим количеством повторений для развития выносливости."
+    },
+    {
+        "exercies_name":"2. Отжимания с отягощением",
+        "src":"https://3.downloader.disk.yandex.ru/preview/ce0a3078e2ed08a039a5add977e1579e259640b42467e68560dd5e3bdb406775/inf/yvr2VZvXeG1XeT3eKeXE4XeycruV_s6tn7qZz96gQOHhK8UjkSM11yO-g8BBahw4I4j9PtbVBg_O3LbSr8aLZg%3D%3D?uid=1687838051&filename=2.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Отжимания от пола — силовое упражнение, с которого начинаются первые шаги в бодибилдинге. Но через несколько месяцев регулярных занятий классический вариант становятся легкими, и на смену ему приходят отжимания с отягощением."
+    },
+    {
+        "exercies_name":"3. Отжимания на упорах",
+        "src":"https://2.downloader.disk.yandex.ru/preview/07942b02cd43e9c888c3faa141186f41b062f5fb3b448e192067014d446c74e9/inf/b4a_fpqxYAOgmnoxIAnZXTvX1ZmihxO4E5R8ka7KfwBCFfACQy0hZ_ufjMTX2GgzBfH_DtdPMB0AsPZCpM1DQw%3D%3D?uid=1687838051&filename=3.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Отжимания — одно из самых распространенных силовых упражнений в мире. А вот отжимания на упорах — его усовершенствованный и не менее эффективный вариант. Это базовое упражнение задействует те же мышцы, но имеет ряд преимуществ."
+    },
+    {
+        "exercies_name":"4. Отжимания с поднятыми ногами",
+        "src":"https://2.downloader.disk.yandex.ru/preview/e2a98e73a880fae9c0c082798e89f822145818d93c13e9bb7eea22093627ca27/inf/vvZ52O61WzgqhJTJU2lzv9WXY7_2udx7Jq1VHN3-67FlGffBcwdzH-U6hjUQtbighYITjSc1jU2JBCtUvpN9tA%3D%3D?uid=1687838051&filename=4.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Отжимания с поднятыми ногами — это усложненный вариант отжиманий от пола. Ноги ставят на возвышенность так, что корпус был под углом к полу, а не параллелен ему. За счет этого нагрузка повышается, а акцент переносится на верх груди."
+    },
+    {
+        "exercies_name":"5. Отжимания с колен",
+        "src":"https://3.downloader.disk.yandex.ru/preview/f14ee242d8e1ee74d0966f0a1c0e6bc4c25b603f1e0c23da7998f13758ec09c6/inf/OYI-Vx6gYgvG2aHlzt2VvNWXY7_2udx7Jq1VHN3-67Eba6p6GCIfvznIGjCTOdXGy1RIvAxBQILSZFM7PuE-vA%3D%3D?uid=1687838051&filename=5.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Отжимания с колен — это вариант классических отжиманий от пола, который отлично подойдет новичкам в качестве более легкой альтернативы. Однако это все то же базовое упражнение для тренировки верхнего плечевого пояса. Его применяют практически в каждой сфере, касающейся физической подготовки."
+    },
+    {
+        "exercies_name":"6. Отжимания на кольцах",
+        "src":"https://3.downloader.disk.yandex.ru/preview/6bbf9e400d37b19b21e85430f2c5c5101afc2b4bdaa6af97d713eaa04fed9be8/inf/OpTOQnBmmdE4Xduck6mUlfk5QCLupP3tbymR7fkVfk1mAhNaJbQD4CHcCdJnXR7SRTgm6Ka60EHlB8b6yMTpUg%3D%3D?uid=1687838051&filename=6.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Отжимания на кольцах — это упражнение для укрепления мышц верхней части тела, которое выполняется на гимнастических кольцах. Прорабатывает грудь, трицепс и плечи, также нагружает мышцы кора. Упражнение считается более сложным вариантом отжиманий на брусьях. Применяется в спортивной гимнастике и кроссфите, но также будет способом усложнения отжиманий для любого спортсмена."
+    },
+    {
+        "exercies_name":"7. Отжимания от стены",
+        "src":"https://2.downloader.disk.yandex.ru/preview/e4c3c2104f3eda509bf67951d1bd6517c7c8513418281590a4fb7ff47de5893c/inf/IWCTImpOjKISN78ca9Vk8asCp-0wTMrZ7oPgyEwulXnsbAdQXGAIQ1zjlGG4BXyElSuI6ILkEwPdtqDnsGjy6g%3D%3D?uid=1687838051&filename=7.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Отжимания от стены — это максимально упрощенный вариант отжиманий от пола. Упражнение доступно для любого уровня физической подготовки. Используется в качестве первого этапа в освоении классических отжиманий."
+    },
+    {
+        "exercies_name":"8. Разведение гантелей лежа на наклонной скамье",
+        "src":"https://2.downloader.disk.yandex.ru/preview/5c98352b501f9a00cfc0ab86469c2e38f413b0a732462b50ea486123555fb754/inf/ydsWVSall2eRGpcEKl3VZ9WXY7_2udx7Jq1VHN3-67EQ1TtidfKhigEdPeSJWMx8qGS6k4OWcWdq_w059vuC6g%3D%3D?uid=1687838051&filename=8.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Разведение гантелей лежа на наклонной скамье — это изолирующее упражнение для проработки верха грудных мышц. Используется для придания груди эстетичной формы. В отличие от жима на наклонной скамье, в упражнении используются меньшие веса, что отлично подходит для многоповторной тренировки."
+    },
+    {
+        "exercies_name":"9. Жим гантелей лежа узким хватом",
+        "src":"https://2.downloader.disk.yandex.ru/preview/ed2496a64164162df3ba37f8776e6dd33346dfa89de06aa655e0305c1157f779/inf/P2m_kpifohCfXcbXOEwndlp_qUVwsBa52b5BYhTLqqmUfAhENB6DxeBket3Uiba3cADRdcc7N6Fno_KYXIcvuw%3D%3D?uid=1687838051&filename=9.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Жим гантелей лежа узким хватом – это вариант жима, в котором ладони направлены друг на друга. В этом случае локти расположены ближе к корпусу, из-за чего нагрузка смещается на трицепс и на внутреннюю часть грудных мышц. Выполняется в низком диапазоне повторений для развития силы, или в более высоком для наращивания массы."
+    },
+    {
+        "exercies_name":"10. Жим штанги лежа на наклонной скамье",
+        "src":"https://3.downloader.disk.yandex.ru/preview/88ca3f77736d981ffb0542a42a1c8cffa98aeea446f7deb98a85c3a305c4425a/inf/RD7_yWyAW6uPhgNRTvPoPmQTKQLFYftcKK2Wf7GSZ5WWqYh4irrV6LFhXnhABN3iyf32mBYwBDecEdzlmtgGQA%3D%3D?uid=1687838051&filename=10.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Жим штанги на наклонной скамье — это базовое упражнение, направленное на проработку груди. Изменение угла наклона скамьи смещает нагрузку со средней части грудных мышц на ее верхние пучки."
+    },
+    {
+        "exercies_name":"11. Жим гантелей лежа на полу",
+        "src":"https://2.downloader.disk.yandex.ru/preview/772db4da9ea104c83d21cc87c0f846c6ca85013f78535e9e58c20a5483af0d3c/inf/r-8xDm2U7bcFhzK4BjhJX2QTKQLFYftcKK2Wf7GSZ5UexCavB8-fWJbCDjIXrgZPwbqu1FFjO3YP5aFv-CgNIw%3D%3D?uid=1687838051&filename=11.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Жим гантелей на полу хоть и ограничивает амплитуду движения, свойственную классическому варианту на скамье, все равно дает хорошую нагрузку на грудные, трицепс и передние дельты. Это упражнение позволит усилить конечную фазу жимового движения и укрепить мышцы-стабилизаторы плечевого пояса."
+    },
+    {
+        "exercies_name":"12. Жим в грудном тренажере сидя",
+        "src":"https://3.downloader.disk.yandex.ru/preview/da20e81524957db196849ce424a3c5279c93ed0b6ea4fa66161b0104bb55855a/inf/LYd25hC4oEgOs3O5n0eSNWQTKQLFYftcKK2Wf7GSZ5XHIC0FieKbKMy5EQuxOF1vQhft9VMS6JcApIMBnDpvVg%3D%3D?uid=1687838051&filename=12.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Жим в грудном тренажере сидя – это базовое многосуставное движение для комплексной прокачки не только груди, но и других мышц плечевого пояса. В отношении набора мышечной массы и роста силы это упражнение немного уступает по эффективности свободным отягощениям. Но становится незаменимым при тренировках на рельеф."
+    },
+    {
+        "exercies_name":"13. Отжимания широким хватом",
+        "src":"https://4.downloader.disk.yandex.ru/preview/b4d6d8adc20d9d5debde1f6349c488b07988f5ce076fc5dc2e5988ad9f537c43/inf/4xw3qkKvMQJ0YUioKJtiT3O2nBS_ByJf9zKmQQGe0mqMlnAjREdy0j1CkQDlNi_65VdrFKG8fYqrdwcvJkOB0Q%3D%3D?uid=1687838051&filename=13.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Отжимания широким хватом — это вариант отжиманий с широкой постановкой рук. Прорабатывает мышцы верхней части тела, включая грудные, плечи и трицепс. Главная нагрузка ложится на грудь. Если в такой постановке вы чувствуете боль в плечах, отдайте предпочтение более узкому варианту."
+    },
+    {
+        "exercies_name":"14. Жим гантелей на наклонной скамье",
+        "src":"https://1.downloader.disk.yandex.ru/preview/bb0a92ce4ad58a4291ad98b4a34b3c8db0e5aa046a3f9b681c557e0323d03c25/inf/ZmlMxb6uy1rAz6kqLXa33SnUACYx_Ppp5d3Fv31jAQm1l0lJ21u-VfwaS0lkGCSEMRl8pzY6VREfYxpmH-RA3Q%3D%3D?uid=1687838051&filename=14.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Жим гантелей на наклонной скамье — это упражнение для мышц плечевого пояса, которое прицельно прорабатывает верх грудного отдела."
+    },
+    {
+        "exercies_name":"15. Отжимания от пола",
+        "src":"https://1.downloader.disk.yandex.ru/preview/2eeb4cf7049af91fb4f5bf9a199517a959bc087823c01ab9aebe304598d43b58/inf/dEXQ9KQcPU8ro5bb1RxZcHO2nBS_ByJf9zKmQQGe0moRlThJJeW0AuMnBTkXj4Z7vgUcIpMRhwuEVQf0VV8E7g%3D%3D?uid=1687838051&filename=15.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Отжимания от пола — это популярное упражнение с собственным весом. Используется для увеличения объемов мышц верхнего плечевого пояса и их силовых показателей. Прорабатывает грудные, трицепсы и плечи, также задействует верх спины и мышцы кора."
+    },
+    {
+        "exercies_name":"16. Берпи",
+        "src":"https://2.downloader.disk.yandex.ru/preview/360bbb7d11265a2242d7273eda7128befd645e1833a81d12615c99475a1538f5/inf/bRslkawIZUGtRPBcPteJRHO2nBS_ByJf9zKmQQGe0mpG8-LZNfcOOawc9SdEaM7NN1hyev3oE33NQFwR_pqmng%3D%3D?uid=1687838051&filename=16.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Берпи – это одно из основных движений, используемых в различных высокоинтенсивных и функциональных методиках. Оно содержит в себе комбинацию последовательно выполняемых упражнений – планки, отжиманий от пола, приседания и прыжка вверх. Упражнение Берпи комплексное. Оно вовлекает в работу практически все основные мышечные группы, повышая энерготраты до максимальных величин. Как раз поэтому движение часто применяется в методиках, направленных на похудение."
+    },
+    {
+        "exercies_name":"17. Пуловер со штангой лежа под отрицательным углом",
+        "src":"https://2.downloader.disk.yandex.ru/preview/43143bc87d1aef352cc313e64341612b4a8bc4d4231db7e3dd87baf0eda412b0/inf/tro3W151R_Q5Q7pFYE8y2C_1lU6vUvjNtLyMv1dd8sJYNDEJiCY2-gG9SGomBBGjMzPHAIgu_QdvrmdjC9MciQ%3D%3D?uid=1687838051&filename=17.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":""
+    },
+    {
+        "exercies_name":"18. Жим лежа на наклонной скамье в тренажере Смита",
+        "src":"https://2.downloader.disk.yandex.ru/preview/75c22c1a26dca9119d6adc77fc0fd7fac619405969e15bb363463b3b1f516e22/inf/Rfv7knTrBtNdUaDLNHbqVC_1lU6vUvjNtLyMv1dd8sImARhfVWvTzae6PCsQbJQQP3c-fTqfG_7ApHMU2DADvA%3D%3D?uid=1687838051&filename=18.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Жим лежа на наклонной скамье в тренажере Смита – это упражнение на грудные мышцы, для проработки верхнего отдела. Повторяет жим на наклонной скамье, но конструкция тренажера фиксирует амплитуду движения. Жим лежа на наклонной скамье в тренажере Смита – это упражнение на грудные мышцы, для проработки верхнего отдела. Повторяет жим на наклонной скамье, но конструкция тренажера фиксирует амплитуду движения."
+    },
+    {
+        "exercies_name":"19. Жим лежа в тренажере Смита",
+        "src":"https://2.downloader.disk.yandex.ru/preview/788ede50b427dd2fc20fd49275d316903c7845db60b7d2eedd4592ce5212163e/inf/G6JBug5IMBNHcIphgLXcfy_1lU6vUvjNtLyMv1dd8sI32ZiV6Ae73miImGDwLUr3NGfNDG2vcb9pR2XNOIkOQQ%3D%3D?uid=1687838051&filename=19.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Жим лежа в тренажере Смита – это упражнение для проработки груди. Похоже на обычный жим штанги лежа, но конструкция тренажера фиксирует амплитуду движения. Малое количество повторений используют при силовых тренировках, большее — для наращивания массы и увеличения силовой выносливости. Обычно выполняется в среднем или высоком диапазоне повторений, от 8-12 за сет, как часть тренировки груди или верха тела."
+    },
+    {
+        "exercies_name":"20. Железный крест в кроссовере",
+        "src":"https://1.downloader.disk.yandex.ru/preview/7c3774e7a9e5e54805c847da0289c69cdca15c5c8d3df2eb6452ca82bf9d8f85/inf/0vyr3t25Mu_u52P4NS71Py_1lU6vUvjNtLyMv1dd8sKcc9_0kwBHgZXkPuOUc_mODJ_B6iq66pq7oFWekUoBFw%3D%3D?uid=1687838051&filename=20.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Железный крест в кроссовере – это изолирующее упражнение в блочном тренажере, для построения больших и сильных грудных мышц. Используя съемные рукояти, можно проработать грудь под разными углами, меняя высоту крепления. Используется как часть тренировки верха тела и мышц груди, часто для предварительного утомления в начале тренировки или как завершающее упражнение. Хорошо сочетается с жимами и разводками, для проработки разных отделов грудных мышц."
+    },
+    {
+        "exercies_name":"21. Жим лежа в тренажере Смита под отрицательным углом",
+        "src":"https://3.downloader.disk.yandex.ru/preview/cd74312db97364d4a720cf6a8369cb16e3c60c4dfdf9981a452b745765167fad/inf/Qe-mV8Mqq7SncH4qApF2SIuESfmAAFkq9sd1uxhCbihfe6FhhA8a0_6J3wB_4b1_-YljomYwvQgIYhPlx6O5UQ%3D%3D?uid=1687838051&filename=21.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Жим лежа в тренажере Смита под отрицательным углом – это упражнение для проработки грудных мышц, с акцентом на нижнюю часть. Движение похоже на обычный жим, но конструкция тренажера обеспечивает фиксированную амплитуду движения. Выполняется в среднем и высоком диапазоне повторений, например 8-12 за сет и более. Используется как часть тренировки груди или мышц верхней части тела."
+    },
+    {
+        "exercies_name":"22. Жим гантелей лежа под отрицательным углом",
+        "src":"https://3.downloader.disk.yandex.ru/preview/ce7b49093022793cea59892a2c942146482324baeb096173f10de5504e86d224/inf/qVplV4AUZ779m4oxytrImouESfmAAFkq9sd1uxhCbigDWAVrnPa1ACw-vOeeZG9uVV2k7n3PmXDZp-Ecf8VFqg%3D%3D?uid=1687838051&filename=22.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Жим гантелей лежа под отрицательным углом – это базовое упражнение на верхнюю часть тела, для проработки нижнего отдела груди. Гантели значительно тяжелее контролировать, чем штангу. "
+    },
+    {
+        "exercies_name":"23. Жим гантелей на наклонной скамье обратным хватом",
+        "src":"https://4.downloader.disk.yandex.ru/preview/5fcba4027057dcafe208aa5fdda4476de0aa54923b3e646025b0775dd77e85c6/inf/F-4xe4c3-S6bovN56JyBH4uESfmAAFkq9sd1uxhCbiij1Gz8Mtg7R-LuMjCZfmgxMu5GkJtPKp45dfjtV8ScKg%3D%3D?uid=1687838051&filename=23.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Жим гантелей на наклонной скамье обратным хватом – это упражнение на верхнюю часть тела, для проработки грудных мышц. При использовании обратного хвата сильнее задействуется верхний отдел мышц груди. Выполняется в среднем и высоком диапазоне повторений, например 8-12 за сет и более. Используется как часть тренировки верха тела или груди."
+    },
+    {
+        "exercies_name":"24. Горизонтальный жим стоя в кроссовере",
+        "src":"https://4.downloader.disk.yandex.ru/preview/93f10a9b1eb40c7bf79b1b655760a0b2eb26b1071e7b6294559adf31916050f5/inf/3Y5WH8nIYI2Uowd3eDDx-dWXY7_2udx7Jq1VHN3-67FM-l4Ljn3KcwGF_w-51RMHvwZUFKWoe0z0eDq-Jdo3zg%3D%3D?uid=1687838051&filename=24.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":""
+    },
+]
