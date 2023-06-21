0acb5de2f5f8d345bdc3ba007e8f8dea0dd3663b
Alex-vvhite <xodyrev.aleksej42@gmail.com>
2023-06-21 04:07:48 +0300

    Add files via upload

---
+++ src/nogi/bicepc_bedra.js
@@ -0,0 +1,73 @@
+const bicepc_bedra = [
+    {
+        "exercies_name":"1. Румынская тяга на одной ноге",
+        "src":"https://4.downloader.disk.yandex.ru/preview/4244375ba6d20b30113a0257ec9427edc7294bb7fe91ab0b08bb8b29cb09760e/inf/71ASsVUDf9xv9XB3wheXkeDqSgxajdYtT8KIKtyLP4myP-1Xb8vvI2e3ZQTqA6S721hR5oQzf5llbR9ZS8BZ4A%3D%3D?uid=1687838051&filename=1.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Румынская тяга на одной ноге — это упражнение, которое относится к базовым движениям. Оно направлено на развитие ягодиц и мышц задней поверхности бедра. Чаще используется в качестве добивочного движения, в конце тренировки, в среднем или высоком диапазоне повторений, для акцентированного воздействия на целевые мышцы."
+    },
+    {
+        "exercies_name":"2. Становая тяга в стиле сумо",
+        "src":"https://1.downloader.disk.yandex.ru/preview/8e79d1eddac6880376e1e138979428bc84a453ec6faeee80aea8fa8734d94471/inf/0mn2Mz2LS41w6FrzBLYnRqlFdO1w0sJQu7l_6GZQ0PxapF6MxidBB2ArYXyPRg8fnqeZXpG8K6BfGEmGoWwlwQ%3D%3D?uid=1687838051&filename=2.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Становая тяга сумо – это базовое упражнение, которое отличается от классического варианта тем, что выполняется с широкой постановкой ног. Прорабатывает бицепсы бедер, ягодицы и мышцы спины. А из-за позиции ног повышенную нагрузку получают мышцы внутренней части бедра."
+    },
+    {
+        "exercies_name":"3. Румынская тяга со штангой",
+        "src":"https://2.downloader.disk.yandex.ru/preview/66333ea0331444dd1fd8983378bc7596da42ded151be32c7c282cf5c1b854d80/inf/jbSv44mLL_3ab3XvPaWMa6lFdO1w0sJQu7l_6GZQ0PzIzz0WQMBfu91NqxYK2XOgQVkpEXQTXKLmpR-kgCZhvw%3D%3D?uid=1687838051&filename=3.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Румынская тяга со штангой — это упражнение, которое прорабатывает бицепсы бедер, ягодицы, а также низ и верх спины. Используется в качестве вспомогательного движения для классической становой тяги, а также подходит для развития мышц задней поверхности бедра."
+    },
+    {
+        "exercies_name":"4. Махи гирей",
+        "src":"https://1.downloader.disk.yandex.ru/preview/d32a64500a63e9a0f76ddb5da67420405b421eec0a670a777a80a3b995fec11b/inf/sPWoe880ka9QYONcqk3RnKlFdO1w0sJQu7l_6GZQ0PwCHsueueYcdVgfs7OZldNauNH-rJ9v4qCxlJOLS24riQ%3D%3D?uid=1687838051&filename=4.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Махи гирей — это упражнение, которое активно используется в кроссфите и других видах функциональных дисциплин. Оно направлено не только на развитие мышц ног и спины, но и на улучшение скорости и силовых показателей."
+    },
+    {
+        "exercies_name":"5. Мертвая тяга со штангой",
+        "src":"https://2.downloader.disk.yandex.ru/preview/caeafca11b689d443dd64d4e3726c4b7d643f9ffc12e1fae3e263fa9b51e50f0/inf/Qe-mV8Mqq7SncH4qApF2SPj99FKGe8Y1Wd3qLdWoT2fV9zHf6-MdbjZKY8Igt7J0AZfNuNvecjMdWwrlOPyzBg%3D%3D?uid=1687838051&filename=5.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Под мрачным названием скрывается всем известное упражнение – становая тяга на прямых ногах. Из-за путаницы в терминологии вокруг упражнения много неразберихи. Хотя на самом деле в его выполнении нет никаких премудростей."
+    },
+    {
+        "exercies_name":"6. Мертвая тяга с гантелями",
+        "src":"https://4.downloader.disk.yandex.ru/preview/b7f44a4f28e613a76b06f6adc0d85129ee4569150755ffbdc1a882ab8f2e5439/inf/dHCitwBtFYHdYVwaIpnEZvj99FKGe8Y1Wd3qLdWoT2exFeHNeA7W6EoG0EbPRZ4vDVcRu-5NczKKlsUZRdclyg%3D%3D?uid=1687838051&filename=6.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Мертвая тяга с гантелями — распространенное упражнение для мышц ног у новичков, особенно среди девушек. Именно с него часто начинают знакомство с многообразием становых тяг. Популярность упражнения объясняется его высокой эффективностью для прокачки ягодиц и бицепсов бедер."
+    },
+    {
+        "exercies_name":"7. Мертвая тяга на одной ноге с использованием гири",
+        "src":"https://3.downloader.disk.yandex.ru/preview/cb207432efda516797fb3dc2bde2694656840bd6323a6a477abd022248a4467a/inf/1keD4UI2bBfMc6iI5yMppalFdO1w0sJQu7l_6GZQ0Pw652ZPGuMfC40vE-xe0L_zIXhR4BFsjigpmHdGFBIU1g%3D%3D?uid=1687838051&filename=7.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Мертвая тяга на одной ноге с использованием гири – это вариация мертвой тяги, в которой одна нога остается на полу, а противоположная отрывается от пола в конечной точке движения. Используется в тренировках нижней части тела, для развития силы и наращивания мышечной массы, также тренирует баланс, силу хвата и мышцы кора."
+    },
+    {
+        "exercies_name":"8. Румынская тяга с гирей",
+        "src":"https://3.downloader.disk.yandex.ru/preview/44975fd93d0f09949716704551ebcc117fcf8fb628c6cc51dd2157a7408c91d8/inf/V9sFkLD7n6JfEJH_QFGzHfj99FKGe8Y1Wd3qLdWoT2fGVK-Duiz2t6-sapgiT-omTuADkWlMO2WNjjEXrawbWA%3D%3D?uid=1687838051&filename=8.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":""
+    },
+    {
+        "exercies_name":"9. Обратная экстензия",
+        "src":"https://3.downloader.disk.yandex.ru/preview/36d18e425a6b7e20329d0b582e5cd28d01154038739c2dc13e04790d958c5e09/inf/nVf80L3-dj7j_9VXqTaPaPj99FKGe8Y1Wd3qLdWoT2dQDeVVyfuUvdFyZ6Vbm83zqzsh78kGYvQu83g5_lFlHw%3D%3D?uid=1687838051&filename=9.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":""
+    },
+    {
+        "exercies_name":"10. Сгибание ног в тренажере лежа",
+        "src":"https://2.downloader.disk.yandex.ru/preview/84cc3335190e9d6404746865513886f7cab15a17106a95e434b495e2d12a4621/inf/QJdpjgWmi_F1vxx-pSGnSGUfzfMtyLdkyX3xRlpd9du_pQ-kkURJIRbaICxdd4t7XgowFFZL2jAvNNgN7PCD9g%3D%3D?uid=1687838051&filename=10.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Сгибание ног в тренажере лежа — это упражнение, которое прорабатывает бицепсы бедер. Как правило, выполняется в среднем или высоком диапазоне повторений, например 8-12 за сет, как часть тренировки ног с предварительным утомлением или в качестве «добивки» в конце занятия."
+    },
+    {
+        "exercies_name":"11. Мертвая тяга на одной ноге с гирей",
+        "src":"https://1.downloader.disk.yandex.ru/preview/f51cb1b239aff37fa4a6b6ffa835e9a7887fcb8f19b41bdc89e28e1dd52aaecb/inf/nQ45cVlq3U1OTyCymy6r8ODqSgxajdYtT8KIKtyLP4kbF2anx2HNSp05VE25XkJrBy3n_VF-qLa8nDaYCAefTg%3D%3D?uid=1687838051&filename=11.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Очень не удобное, но очень эффективное упражнение. Неудобство его, как вы понимаете, в том, что равновесие удержать в таком положении достаточно сложно. Поэтому для начала следует поработать с легкой гирей и привыкнуть к упражнению. Мертвая тяга гири работает также как и аналогичная тяга со штангой или двумя гантелями. Основную нагрузку принимают бицепсы бедер, второстепенную — мышцы низа спины. Отличие лишь в том, что вся нагрузка идет на одну ногу. Эта особенность делает упражнение интенсивнее своих аналогов."
+    },
+    {
+        "exercies_name":"12. Сгибание ног стоя",
+        "src":"https://1.downloader.disk.yandex.ru/preview/874c1cf6056d7e3974ca9d4891792c127359160692817aedb45a2f907c16b2f9/inf/3AO420XuFOfu0sUJC6dcQuDqSgxajdYtT8KIKtyLP4m5XaI8Wao_gU3BM1XJRuTAD8BSqd9ppwqNF_SmZ9woJw%3D%3D?uid=1687838051&filename=12.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Главной задачей сгибаний ног стоя является изолированная прокачка тыла ваших бедер, иными словами их бицепсов. Нагрузка действует также как и при аналогичных сгибаниях ног лежа, но практически не создает напряжения в пояснице. В бодибилдинге частенько практикуется прием, когда сгибания стоя выполняют следующим упражнением после варианта лежа. Это немного меняет угол воздействия нагрузки на мышцу, но самое главное — разгружает, напряженную предыдущим упражнением, поясницу."
+    },
+    {
+        "exercies_name":"13. Сгибание ног сидя",
+        "src":"https://1.downloader.disk.yandex.ru/preview/4f23c41b7463023146dfa5f3a5f6810b3e2ba92393388cad8cbeefeba724e3eb/inf/BHfyqU1_r49QuWzOnKAHPeDqSgxajdYtT8KIKtyLP4ll0PIVgSzhqJa5zMZeM5tUScCghmS_A5fd3xHqJMZi4w%3D%3D?uid=1687838051&filename=13.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Данная разновидность сгибаний ног является менее популярной чем аналогичное упражнение в положении лежа. Все также трудятся мышцы задней поверхности бедер, но угол воздействия нагрузки незначительно меняется. Поэтому особой цели в замене лежачего варианта на этот нет, разве что для разнообразия воздействия нагрузки на бицепсы бедер."
+    },
+    {
+        "exercies_name":"14. Тяга на прямых ногах с гантелями",
+        "src":"https://2.downloader.disk.yandex.ru/preview/b0918159c76a125564467a7f140a566d9fe366a541903b4ebc7aa0831eca0ae4/inf/frxFwExjCCYPkRPdfP36FalFdO1w0sJQu7l_6GZQ0Pw6PBvL0yenmpJqPy1ngX7VJ_Ju4n3J8Yx7CUwTynezxw%3D%3D?uid=1687838051&filename=14.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Ягодицы и задняя поверхность бедра — вот те мышцы, которые принимают на себя основной удар нагрузки от этого упражнения. Менее значимой нагрузке подвергаются мышцы низа спины. Чтобы такой баланс не изменялся, хорошенько сосредоточьтесь и следите за тем, чтобы движение осуществлялось только за счет разгибания тазобедренного сустава. В этом и есть вся фишка, не нужно тянуть вес спиной. Если вы не чувствуете нагрузку в ногах, значит В отличии от тяги со штангой, вариант с гантелями позволит вам увеличить амплитуду движения, это раз и задаст более правильную, с точки зрения физиологии траекторию нагрузки, это два. Главное держите их не перед собой, а по бокам от туловища."
+    },
+    
+]
