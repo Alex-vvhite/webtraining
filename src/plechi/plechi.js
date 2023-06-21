cd032fe57bb6b25f8ff903b77a353f5e0dd259fd
Alex-vvhite <xodyrev.aleksej42@gmail.com>
2023-06-21 04:08:31 +0300

    Add files via upload

---
+++ src/plechi/plechi.js
@@ -0,0 +1,137 @@
+const plechi = [
+    {
+        "exercies_name":"1. Отжимания в стойке на руках",
+        "src":"https://1.downloader.disk.yandex.ru/preview/9d977ea01c322ad35c5fd26f07ba75832568e3f2bd2d667f20bc3919cffb823b/inf/fLaPImrt4vKBd29q186KYCaEFJnvDr_f3WCyxQmoOXI29ae2yrdfAbhxL8tRVGn_grjHSa38IiEpFFh7l47rOQ%3D%3D?uid=1687838051&filename=1.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Вертикальные отжимания в стойке на руках — это упражнение на мышцы плеч. Также в движении участвуют трицепсы."
+    },
+    {
+        "exercies_name":"2. Трастеры с гантелями",
+        "src":"https://4.downloader.disk.yandex.ru/preview/6c86a826400898b2b056059072356be980c5ca1335d31a88c11cf8972ba57fef/inf/9LNC8Jpy7EX1b53loAldXqjlW9svj-pKvo8WFETNv7f4dLEardf9_rsf_VbQ1RRUAM0VDsjgK9WLPBGq2xmRXQ%3D%3D?uid=1687838051&filename=2.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Трастеры с гантелями — комбинированное упражнение, которое прорабатывает все тело одним последовательным движением. Оно начинается с приседа и заканчивается жимом гантелей стоя. Выполняется с гантелями, гирями или небольшой штангой."
+    },
+    {
+        "exercies_name":"3. Жим Арнольда",
+        "src":"https://4.downloader.disk.yandex.ru/preview/ce2775fb9fe042dce83ba467e676871529761b4dd9822acf3690a46dee8d5434/inf/4q1fS8c5tKti7osXq8X3lhX6B_uxL9DiifbR2EB5c4FCCmBKVgQFEpTpvPNWxKgZmKU2Q8RW7kRa12NA7oRHfQ%3D%3D?uid=1687838051&filename=3.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Жим Арнольда назван в честь иконы бодибилдинга и кинозвезды Арнольда Шварценеггера. Атлет активно использовал это упражнение в своих тренировках и добился хорошего прогресса в развитии дельтовидных мышц. К классическому жиму гантелей над головой здесь добавлено вращение кистей, что позволяет проработать все три пучка дельт в одном движении."
+    },
+    {
+        "exercies_name":"4. Попеременные махи гантелями вперед и в стороны",
+        "src":"https://3.downloader.disk.yandex.ru/preview/7c65b9e7caf293cb653e61562a2c4fc5707afa4a93fdb39355d74b19aa0b8e31/inf/Z9YN8nVGO9BiH_LOEWh1SRX6B_uxL9DiifbR2EB5c4HjA8y0SJH8SU6zd1dEf9Z068eYlaf2rv4YbXP9maujJA%3D%3D?uid=1687838051&filename=4.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Попеременные махи гантелями вперед и в стороны – это комплексное движение, объединяющее два упражнения. Прорабатывает передние и средние дельтовидные мышцы. Обычно выполняется один повтор для каждого движения, также можно постепенно увеличивать повторы в последующем раунде (например: 1 повтор в первом раунде, 2 во втором и так далее). Какой бы способ вы не выбрали, нагрузку получат как передние, так и средние дельты, что позволит сэкономить время. Упражнение используется в интенсивных тренировках плеч, как вспомогательное движение, или как часть тренировки верха тела."
+    },
+    {
+        "exercies_name":"5. Подъем штанги на грудь и жим",
+        "src":"https://2.downloader.disk.yandex.ru/preview/77a6dd9e267d02564448ea78ed8f21e91d657359caa233d8406f329da5dc0bf3/inf/_K4Lf6tP8w0dWFpIf24OGhX6B_uxL9DiifbR2EB5c4HGp67kT-5v7vT637z8Lo668NRj0rv0vtR4CfC-TsvulA%3D%3D?uid=1687838051&filename=5.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Подъем штанги на грудь и жим – это упражнение на всё тело, состоящие из двух движений: подъём на грудь, в котором штанга поднимается на грудь трехсоставным рывком и последующий жим штанги над головой. Когда-то упражнение было соревновательным на Олимпиаде. В наши дни, это стоящее упражнение для взрывных и силовых тренировок на мышцы всего тела. Выполняется от малого до высокого диапазона повторений. "
+    },
+    {
+        "exercies_name":"6. Жим гантели одной рукой",
+        "src":"https://3.downloader.disk.yandex.ru/preview/bd0f132b0af2a96780f79d948dac0c40f69da692591b78904cb1c54f59f56948/inf/grg1_I6JPuO7N-qY72DU1uegYGEGYtbxlmnga25MHE0gXDCj20Kk15v0N1UkCVAxBP-LTVsY796YiMRRwJ2-9A%3D%3D?uid=1687838051&filename=6.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Жим гантели стоя одной рукой – это упражнения для увеличения силы и объема плеч. Основными отличиями от классического жима гантелей сидя, являются вертикальная позиция тела и использование одновременно только одной руки, что позволяет поднимать более тяжелые веса."
+    },
+    {
+        "exercies_name":"7. Разводка в кроссовере на заднюю дельту стоя",
+        "src":"https://3.downloader.disk.yandex.ru/preview/b1b3dd3522d100bf22af78eda74bb2b73dad65f8917f8f85b5ee2efc1b242fce/inf/Cs9bww2JgOTEYCbb1o_h6qjlW9svj-pKvo8WFETNv7el9s_cXSiXTL98pXA3SxUFFRJPo4lvE64AxP_AN61uOw%3D%3D?uid=1687838051&filename=7.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Задний пучок дельтовидной мышцы — относительно небольшая и слабая мышца. Но именно от ее уровня развития зависит эстетический вид плеча при взгляде сбоку. Разводка в кроссовере стоя — упражнение, направленное на изолированную проработку задней дельты. Выполняется в среднем или высоком количестве повторений при тренировке верха тела или плеч."
+    },
+    {
+        "exercies_name":"8. Мельница с гирями",
+        "src":"https://2.downloader.disk.yandex.ru/preview/19293164669a10ac931735f435685c948c1c736290ba5ab3a02b6349bf11441f/inf/jbSv44mLL_3ab3XvPaWMawryv_SqG3fhxKC0JK4B-KwF8YdsJBW530jonKve1xnx_IJ_fqNtxK7LpLumfwVUtw%3D%3D?uid=1687838051&filename=8.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Мельница с гирями – это упражнение высокого уровня сложности, прорабатывает силу мышц кора и плеч, а также подвижность и устойчивость бедер. Поскольку вы одновременно удерживаете одну гирю над головой, а вторую у пола, силовую и статическую нагрузку получают мышцы кора и плечи. Лучше не использовать в оздоровительных тренировках и при сильной усталости. "
+    },
+    {
+        "exercies_name":"9. Вращение рук с дисками",
+        "src":"https://2.downloader.disk.yandex.ru/preview/25bf1fbcd9e7c7504ec571a096a38b86350586a180d0ebe8d0df7884a2eb7837/inf/HXXK3JGF4-pHH7QlKGowRQryv_SqG3fhxKC0JK4B-KwbU6-lrrFJH71K_58o1BsOAEXydnnrNz3lgr3mGxPEZQ%3D%3D?uid=1687838051&filename=9.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Вращение рук с дисками – это упражнение для плеч. Как правило, используется для разминки, а не как отдельное упражнение. Прогревает плечевой сустав и задействует труднодоступные мышцы-вращатели."
+    },
+    {
+        "exercies_name":"10. Подъем ногами по стене",
+        "src":"https://2.downloader.disk.yandex.ru/preview/95eb24e2c5b07d1c20d610fe34188c4b7143a24574dbd57efa376daad3c7a45a/inf/ABVvwF7X42guq2O5VVFOAGbS1z9NqLJOZMco6J39xOeA9tYSk6JUNsbeZyGwnBWOtLHdCeTIiXAyb1n-UMVjZg%3D%3D?uid=1687838051&filename=10.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Подъем ногами по стене – это достаточно сложное упражнение на все мышцы тела. Из упора лёжа руки шагают назад, пока ноги заходят на стену, затем упражнение повторяется в обратном порядке. Новичкам придётся долго тренироваться, чтобы освоить упражнение. "
+    },
+    {
+        "exercies_name":"11. Подъем плеч со штангой на наклонной скамье",
+        "src":"https://1.downloader.disk.yandex.ru/preview/0b8304750f3c4b7dd61dfa02f0e1d66f86eed5a032c4e5c8addf5ef2801b1046/inf/lDmSKYUx2bWWwHF_FyJYGYTdKCmCIkyVVhKkfls2s9xxiTlQJoV3uswiWn6VS_wRx2zqMI1q5Z-fQtyas_FKzg%3D%3D?uid=1687838051&filename=11.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Подъем плеч со штангой на наклонной скамье – это упражнение на верхнюю часть тела, для улучшения подвижности плеч и верха спины. Поможет усилить жимовые движения и предотвратить травмы. "
+    },
+    {
+        "exercies_name":"12. Фронтальный подъем штанги над головой",
+        "src":"https://2.downloader.disk.yandex.ru/preview/11e698567738f962ce28de3f8e5b185c923d2fe3b35afc367bc6ee91656a212a/inf/4NNPapHC3ZotneSNoqQnGITdKCmCIkyVVhKkfls2s9y5qybpY6zK_u8tGsZ3FNJxFJ8J_PeDJpQrOPuj8Tnb6g%3D%3D?uid=1687838051&filename=12.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Фронтальный подъем штанги над головой – это упражнение для проработки передних дельт, а также трапеций, особенно верхней части. Можно выполнять с диском или гантелью, прямой хват более щадящий для плеч. Обычно используют в многоповторной тренировке (8-12 повторов за сет и больше) трапеций или плеч, для наращивания массы."
+    },
+    {
+        "exercies_name":"13. Тяга к подбородку в тренажере Смита (одной рукой)",
+        "src":"https://3.downloader.disk.yandex.ru/preview/d19a49be3e1e44b061e8dc279732913a2968c670975c2f8fd032630cb37517d6/inf/wZEgurWbKpKNPx4g81SnZOegYGEGYtbxlmnga25MHE1LB8oQq6bIajM3xKhqZilccT7hpcDrwzOzb3qh_k1JaQ%3D%3D?uid=1687838051&filename=13.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Тяга к подбородку в тренажере Смита – это упражнение на мышцы верхней части тела, для проработки трапеций и плеч. Выполняется в высоком и среднем диапазоне повторений, например 8-12 за сет и более. Используется как часть тренировки верха тела или плеч."
+    },
+    {
+        "exercies_name":"14. Подъем штанги перед собой",
+        "src":"https://1.downloader.disk.yandex.ru/preview/97086a19be06565bbe20a7526992ccecd3f95cafa2b363f576812de94d2fe1fb/inf/Egr3_Dlwpj5eeQdLz50rKoTdKCmCIkyVVhKkfls2s9w9LzibVWmq_LCC-VyRvS4mLMMvpb0EfnDXCRFX6TQVFw%3D%3D?uid=1687838051&filename=14.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Фронтальный подъем штанги перед собой – это изолирующее упражнение для мышц верхней части тела, для проработки плеч. Выполняется в среднем и высоком диапазоне повторений, например 8-12 за сет и больше. Используется как часть тренировки плеч. "
+    },
+    {
+        "exercies_name":"15. Жимовой швунг с гирями",
+        "src":"https://3.downloader.disk.yandex.ru/preview/72e295cffa296eadffaf21cfc78e94db19bc4c82a84cc22884578a677e959a13/inf/izHAYG6B8YsUQypNsmZx5iaEFJnvDr_f3WCyxQmoOXLW5qB4gnZW6kgkeyDHNs9XpxbQn-NYehu-HG05E-OtzQ%3D%3D?uid=1687838051&filename=15.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Жимовой швунг с гирями – это популярное упражнение в котором выполняется жим стоя с помощью мышц нижней части тела. Каждый сет начинает с взятия гирь на грудь, опционально можно выполнять это движение в каждом повторе. Можно использовать в силовом или массонаборном диапазоне повторений, в круговых и жиросжигающих тренировках, или как часть комплексной тренировки с гирями."
+    },
+    {
+        "exercies_name":"16. Рывок двух гирь",
+        "src":"https://4.downloader.disk.yandex.ru/preview/30fad3be986231fbdb5aef2f7f73dbeb91df0b327b0b2f25c2fe9392809b13e5/inf/4NNPapHC3ZotneSNoqQnGKGZdq_oyyElXbwjndHtmbtdtt4tA3oZd-K47AwvQ353RS2L-MIeLtrvEVSg-kAXvQ%3D%3D?uid=1687838051&filename=16.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":""
+    },
+    {
+        "exercies_name":"17. Подъем плеч на наклонной скамье в тренажере Смита",
+        "src":"https://4.downloader.disk.yandex.ru/preview/57a0ddd891c7b66a8463b465487d43c71675ab56bec36cb91a91a2d3c48c70d2/inf/r-8xDm2U7bcFhzK4BjhJX6GZdq_oyyElXbwjndHtmbvBQJyLUR6CWpOKQQW_Zy1PyOK4V8r8r1lLPln8-QVtXg%3D%3D?uid=1687838051&filename=17.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":""
+    },
+    {
+        "exercies_name":"18. Обратные разведения лежа с вращением",
+        "src":"https://1.downloader.disk.yandex.ru/preview/21d1f541d11d507b99a9fd8e14a52f89967f9ee6dadfda6d985463e2c7408371/inf/svkfXO3DH5GdDSwtgCIRlaGZdq_oyyElXbwjndHtmbuIpdozKzBLZwlkTsBnAOHGP1_jdt4iLr8X-xYGbvjuFQ%3D%3D?uid=1687838051&filename=18.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":""
+    },
+    {
+        "exercies_name":"19. Обратные разведения лежа на горизонтальной скамье",
+        "src":"https://4.downloader.disk.yandex.ru/preview/346416ad073f2e3bc2a55daf1727e3a0cd7ef59e684b5d89bd7f2888aeee094e/inf/P1vgfbJLJHl00QBbb7lQC6GZdq_oyyElXbwjndHtmbshlBIZio6xnpQyEqPlIUbhpcwK89fYAu7vFMnoD0ACOw%3D%3D?uid=1687838051&filename=19.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Обратные разведения лежа на горизонтальной скамье – это упражнение на верхнюю часть тела, для проработки задних дельтовидных мышц. Лежа лицом вниз вы исключаете возможность использовать инерцию для выполнения движения. Выполняется в среднем и высоком диапазоне повторений, например 8-12 за сет. Используется как часть тренировки верха тела или плеч."
+    },
+    {
+        "exercies_name":"20. Жим Брэдфорда сидя",
+        "src":"https://2.downloader.disk.yandex.ru/preview/5befad04f4802c8df0417fab83fbdfd67bba37cdb79e529a4717231629aa461e/inf/Rzi-ib20Igi63gzaIRPJKKGZdq_oyyElXbwjndHtmbum_5HGYuSLawrT23B8zvmWSBDsFPuXl5QAcHzGQVE8DA%3D%3D?uid=1687838051&filename=20.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Жим Брэдфорда сидя — это базовое упражнение для комплексной проработки передних и средних частей дельтовидных мышц. Вариант сидя немного снижает нагрузку на позвоночник, а также исключает помощь ног при выполнении жима."
+    },
+    {
+        "exercies_name":"21. Махи одной рукой на нижнем блоке",
+        "src":"https://4.downloader.disk.yandex.ru/preview/bca0260ba2577a7f74fc5377875d7b1698eeefb8f995a30e65fb8b72ffe9e68d/inf/Zxzc9G-1G2ewowjf75mwiGUfzfMtyLdkyX3xRlpd9dsBHv3pYe2FaO-eRiN7ll2RpRJGyfefHbUoAfrkZ3UrGQ%3D%3D?uid=1687838051&filename=21.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Махи одной рукой на нижнем блоке – это изолирующее упражнение на плечи, для проработки средних дельт. Второстепенно задействует трапеции и верх спины. Позволяет сфокусироваться на каждой стороне по-отдельности. Выполняется в среднем и высоком диапазоне повторений, например 8-12 за сет. Используется как часть тренировки верха тела или плеч. "
+    },
+    {
+        "exercies_name":"22. Тяга штанги в наклоне к груди",
+        "src":"https://1.downloader.disk.yandex.ru/preview/b3ac1a355174c210e938759490106ecaafb1ba8635a64318cc84b065dc474c08/inf/Cr5MfYyjzespnw2dahuW2KGZdq_oyyElXbwjndHtmbtGlqEJ4JE5CAeoQEw2qhfu4ZFkUKPV4EiNg4osN408vg%3D%3D?uid=1687838051&filename=22.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Тяга штанги в наклоне к груди – это упражнение для проработки плеч и мышц верха спины. В упражнении используются более легкие веса и большее количество повторений, чем в традиционной тяге к животу. Обычно выполняют 8-15 повторений за сет, как часть тренировки спины или мышц верхней части тела. "
+    },
+    {
+        "exercies_name":"23. Отведение руки назад в блочном тренажере стоя",
+        "src":"https://3.downloader.disk.yandex.ru/preview/cde5d4fdcc77fc1a4d47122d610ec568ea54bcb357fcb5ae038fd3db4b9fe751/inf/0O9642c1mnIgKysJxzLUKhX6B_uxL9DiifbR2EB5c4FnqaYDLN-qjs2WCTUASzAfDN_Qzzlp-Rzwah6-Jq8myg%3D%3D?uid=1687838051&filename=23.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Отведение руки назад в блочном тренажере стоя – это упражнение на задние дельты. Используется с целью изолировать и попеременно проработать заднюю часть плеча. Выполняется в среднем или высоком диапазоне повторений, от 8-12 за сет, как часть тренировки плеч или верха тела. "
+    },
+    {
+        "exercies_name":"24. Жим Брэдфорда стоя",
+        "src":"https://2.downloader.disk.yandex.ru/preview/72b4508bb178a8d78e6848d0db64ef213c67b9805a08a1f14d575bda244d0b5e/inf/jr3dKau_Hl9mYsY2EPGQQBX6B_uxL9DiifbR2EB5c4Ga-FUnDWn20-z0T8Z8f7j5Rn5OTILHVCsUwM76HEAJtA%3D%3D?uid=1687838051&filename=24.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Жим Брэдфорда стоя – это эффективное, но забытое упражнение для плеч. В нём чередуются жим стоя с груди и из-за головы. Можно использовать в качестве разминки или как отдельное упражнение в тренировочной программе на плечи или верхней части тела, для увеличения силы и массы дельтовидных мышц. Обычно выполняют в многоповторном варианте, например 8-15 за сет и больше, или до отказа. "
+    },
+    {
+        "exercies_name":"25. Тяга гантелей в наклоне на задние дельты",
+        "src":"https://1.downloader.disk.yandex.ru/preview/aef0cd751bdc287527d580468981f4d1e5d0b8a97ccd95bb5d48d4b7349dec0b/inf/cUJ8qZio3VG8bUwb_OSnjhX6B_uxL9DiifbR2EB5c4FGbiu2Kwjnfobigx2pW7I3KcbcdZVJj_0r6mcFS9bA5w%3D%3D?uid=1687838051&filename=25.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Тяга гантелей в наклоне на задние дельты – это упражнение для тыльной части плечевого пояса. Поскольку в жимовых движениях больше задействованы передние и средние (в меньшей степени) дельты, это упражнение используется для построения пропорциональных плеч. "
+    },
+    {
+        "exercies_name":"26. Вращение диска на прямых руках",
+        "src":"https://3.downloader.disk.yandex.ru/preview/ec67eaf179b34eb42081f1963b6010b08cc62509384efaa3f106db846b9a5753/inf/HDy0rpj0I3dVaC0G_OJh3ej4hCGQFjnnga8XnyWM4q8qy5hHP6FytTcpJk-HKi2yn8Lm_fYYS7smOpHqSHHREw%3D%3D?uid=1687838051&filename=26.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Вращение диска на прямых руках или «водитель» – это упражнение похожее на подъем диска перед собой, разница заключается в том, что атлет вращает снаряд, удерживая его на вытянутых руках, как руль автомобиля. Упражнение укрепляет плечи, в частности передние дельты. Мышцы кора и верха спины тоже задействованы в упражнении. Используется в тренировках на наращивание массы плеч, как часть тренировки верха тела или круговой на всё тело."
+    },
+    {
+        "exercies_name":"27. Толчок грифа одной рукой",
+        "src":"https://3.downloader.disk.yandex.ru/preview/c205a042137c838f7a04dd3033a986a03815eab4c19230ec639f7c901bf93b0c/inf/5LMXFprw7e06gkXn2KT2DBX6B_uxL9DiifbR2EB5c4EKL_MQxRLweYTA7SBaN_3_W285_FrO7KXXZFUGobNtIA%3D%3D?uid=1687838051&filename=27.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=1687838051&tknv=v2&size=1898x942",
+        "about":"Толчок грифа одной рукой – это взрывное упражнение, выполняется с помощью грифа закрепленного в опору на полу или в углу зала, в последнем случае, лучше обмотать край грифа в полотенце, чтобы не повредить стены. В этом упражнении основная задача оттолкнуть гриф от груди одной рукой, и перехватить его в воздухе другой, и так продолжать чередовать стороны. Движение в основном прорабатывает дельтовидные мышцы. Можно выполнять в привычном диапазоне повторений для увеличения силы и набора массы или тренировать взрывную силу или в многоповторном диапазоне."
+    },
+]
