3ec8754d643d17dbe701cc76089beab4af819014
Alex-vvhite <xodyrev.aleksej42@gmail.com>
2023-06-21 04:59:56 +0300

    Add files via upload

---
+++ src/work_execries.js
@@ -0,0 +1,119 @@
+let exerciesData="";
+bottomBack.forEach(item=>{
+    exerciesData +=`<div class="border_blockk">`;
+    exerciesData += `<h3 class="name-exercies"> ${item.exercies_name} </h3>`;
+    exerciesData += `<img class="image_exercies" src="${item.src}" alt="">`;
+    exerciesData += `<p class="name-exercies"> ${item.about} </p>`;
+    exerciesData +=`</div>`;
+})
+document.querySelector(".pouasnica-list").innerHTML = exerciesData;
+
+let exerciesData2="";
+shirochaishie.forEach(item=>{
+    exerciesData2 +=`<div class="border_blockk">`;
+    exerciesData2 += `<h3 class="name-exercies"> ${item.exercies_name} </h3>`;
+    exerciesData2 += `<img class="image_exercies" src="${item.src}" alt="">`;
+    exerciesData2 += `<p class="name-exercies"> ${item.about} </p>`;
+    exerciesData2 +=`</div>`;
+})
+document.querySelector(".shirochaishie-list").innerHTML = exerciesData2;
+
+let exerciesData3="";
+trapecii.forEach(item=>{
+    exerciesData3 +=`<div class="border_blockk">`;
+    exerciesData3 += `<h3 class="name-exercies"> ${item.exercies_name} </h3>`;
+    exerciesData3 += `<img class="image_exercies" src="${item.src}" alt="">`;
+    exerciesData3 += `<p class="name-exercies"> ${item.about} </p>`;
+    exerciesData3 +=`</div>`;
+})
+document.querySelector(".trapecii-list").innerHTML = exerciesData3;
+
+let exerciesData4="";
+sheya.forEach(item=>{
+    exerciesData4 +=`<div class="border_blockk">`;
+    exerciesData4 += `<h3 class="name-exercies"> ${item.exercies_name} </h3>`;
+    exerciesData4 += `<img class="image_exercies" src="${item.src}" alt="">`;
+    exerciesData4 += `<p class="name-exercies"> ${item.about} </p>`;
+    exerciesData4 +=`</div>`;
+})
+document.querySelector(".sheya-list").innerHTML = exerciesData4;
+
+let exerciesData5="";
+yagodici.forEach(item=>{
+    exerciesData5 +=`<div class="border_blockk">`;
+    exerciesData5 += `<h3 class="name-exercies"> ${item.exercies_name} </h3>`;
+    exerciesData5 += `<img class="image_exercies" src="${item.src}" alt="">`;
+    exerciesData5 += `<p class="name-exercies"> ${item.about} </p>`;
+    exerciesData5 +=`</div>`;
+})
+document.querySelector(".yagodici-list").innerHTML = exerciesData5;
+
+let exerciesData6="";
+press.forEach(item=>{
+    exerciesData6 +=`<div class="border_blockk">`;
+    exerciesData6 += `<h3 class="name-exercies"> ${item.exercies_name} </h3>`;
+    exerciesData6 += `<img class="image_exercies" src="${item.src}" alt="">`;
+    exerciesData6 += `<p class="name-exercies"> ${item.about} </p>`;
+    exerciesData6 +=`</div>`;
+})
+document.querySelector(".press-list").innerHTML = exerciesData6;
+
+let exerciesData7="";
+plechi.forEach(item=>{
+    exerciesData7 +=`<div class="border_blockk">`;
+    exerciesData7 += `<h3 class="name-exercies"> ${item.exercies_name} </h3>`;
+    exerciesData7 += `<img class="image_exercies" src="${item.src}" alt="">`;
+    exerciesData7 += `<p class="name-exercies"> ${item.about} </p>`;
+    exerciesData7 +=`</div>`;
+})
+document.querySelector(".plechi-list").innerHTML = exerciesData7;
+
+let exerciesData8="";
+bicepc.forEach(item=>{
+    exerciesData8 +=`<div class="border_blockk">`;
+    exerciesData8 += `<h3 class="name-exercies"> ${item.exercies_name} </h3>`;
+    exerciesData8 += `<img class="image_exercies" src="${item.src}" alt="">`;
+    exerciesData8 += `<p class="name-exercies"> ${item.about} </p>`;
+    exerciesData8 +=`</div>`;
+})
+document.querySelector(".bicepc-list").innerHTML = exerciesData8;
+
+let exerciesData9="";
+tricepc.forEach(item=>{
+    exerciesData9 +=`<div class="border_blockk">`;
+    exerciesData9 += `<h3 class="name-exercies"> ${item.exercies_name} </h3>`;
+    exerciesData9 += `<img class="image_exercies" src="${item.src}" alt="">`;
+    exerciesData9 += `<p class="name-exercies"> ${item.about} </p>`;
+    exerciesData9 +=`</div>`;
+})
+document.querySelector(".tricepc-list").innerHTML = exerciesData9;
+
+let exerciesData10="";
+predplechie.forEach(item=>{
+    exerciesData10 +=`<div class="border_blockk">`;
+    exerciesData10 += `<h3 class="name-exercies"> ${item.exercies_name} </h3>`;
+    exerciesData10 += `<img class="image_exercies" src="${item.src}" alt="">`;
+    exerciesData10 += `<p class="name-exercies"> ${item.about} </p>`;
+    exerciesData10 +=`</div>`;
+})
+document.querySelector(".predplechie-list").innerHTML = exerciesData10;
+
+let exerciesData11="";
+grud.forEach(item=>{
+    exerciesData11 +=`<div class="border_blockk">`;
+    exerciesData11 += `<h3 class="name-exercies"> ${item.exercies_name} </h3>`;
+    exerciesData11 += `<img class="image_exercies" src="${item.src}" alt="">`;
+    exerciesData11 += `<p class="name-exercies"> ${item.about} </p>`;
+    exerciesData11 +=`</div>`;
+})
+document.querySelector(".grud-list").innerHTML = exerciesData11;
+
+let exerciesData12="";
+bicepc_bedra.forEach(item=>{
+    exerciesData12 +=`<div class="border_blockk">`;
+    exerciesData12 += `<h3 class="name-exercies"> ${item.exercies_name} </h3>`;
+    exerciesData12 += `<img class="image_exercies" src="${item.src}" alt="">`;
+    exerciesData12 += `<p class="name-exercies"> ${item.about} </p>`;
+    exerciesData12 +=`</div>`;
+})
+document.querySelector(".bicepc_bedra-list").innerHTML = exerciesData12;
